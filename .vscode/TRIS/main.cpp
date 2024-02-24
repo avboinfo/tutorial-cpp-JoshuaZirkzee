6d5923ae78dc87f3e97871f3a1a0305f17b5762a
JoshuaZirkzee <153986382+JoshuaZirkzee@users.noreply.github.com>
2024-02-24 11:35:11 +0000

    TRIS finale

--- .vscode/TRIS/main.cpp
+++ .vscode/TRIS/main.cpp
@@ -1,196 +1,88 @@
-/*
-se in griglia c'è 0 cella vuota
-se in griglia c'è 1 styiamo parlando di una x(giocatore 1)
-se in griglia c'è 2 stiamo parlando di un cerchio( giocatore 2)
-*/
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
-class Tris
-{
-public:
-    int griglia[3][3];
+class Tris {
+private:
+    const int L = 3;
+    vector<vector<string>> board;
 
-    void reset_tabella()
-    {
-        for (int i = 0; i < 3; i++)
-        {
-            for (int j = 0; j < 3; j++)
-            {
-                griglia[i][j] = 0;
-            }
-        }
-    }
-    void stampa_griglia()
-    {
-        for (int i = 0; i < 3; i++)
-        {
-            for (int j = 0; j < 3; j++)
-            {
-                cout << griglia[i][j] << "\t";
+public:
+    Tris() : board(L, vector<string>(L, " ")) {}
+
+    void printBoard() {
+        for (int i = 0; i < L; i++) {
+            for (int j = 0; j < L; j++) {
+                cout << board[i][j];
+                if (j < L - 1) cout << " | ";
             }
             cout << endl;
+            if (i < L - 1) cout << "---------" << endl;
         }
     }
 
-bool giocatore_uno(int x, int y)
-{
-    if (x > 2 || x < 0)
-        return false;
-
-    if (y > 2 || y < 0)
-        return false;
-
-    if (griglia[x][y] == 1 || griglia[x][y] == 2)
-        return false;
-
-    griglia[x][y] = 1;
-    return true;
-
-}
-
-bool giocatore_due(int x, int y)
-{
-    if (x > 2 || x < 0)
-    return false;
-
-    if (y > 2 || y < 0)
-        return false;
-
-    if (griglia[x][y] == 1 || griglia[x][y] == 2)
-        return false;
-
-    griglia[x][y] = 2;
-    return true;
-}
-
-int controlla_vincitore()
-{
-    int risultato;
-    for (int i = 0; i < 3; i++)
-    {
-        risultato = controlla_colonna(i);
-        if (risultato != 0)
-            return risultato;
-        
-        risultato = controlla_riga(i);
-        if (risultato !=0)
-            return risultato;
-        
-    }
-    
-}
-
-private:
-    int controlla_colonna(int colonna){
-        int acc_uno = 0;
-        int acc_due = 0;
-        for (int i = 0; i < 3; i++)
-        {
-            int cella = griglia[i][colonna];
-            if (cella == 1)
-                acc_uno++;
-            else if (cella == 2)
-                acc_due++;
+    bool placeSymbol(int x, int y, string symbol) {
+        if (x < 0 || x >= L || y < 0 || y >= L || board[x][y] != " ") {
+            return false;
         }
-        if (acc_uno == 3)
-            return 1;
-        
-        if (acc_due == 3)
-            return 2;
-        
-        return 0;
-        
-        
+        board[x][y] = symbol;
+        return true;
     }
 
-    int controlla_riga(int riga){
-        int acc_uno = 0;
-        int acc_due = 0;
-        for (int i = 0; i < 3; i++)
-        {
-            int cella = griglia[riga][i];
-            if (cella == 1)
-                acc_uno++;
-            else if (cella == 2)
-                acc_due++;
+    int checkWinner() {
+        // Check rows, columns, and diagonals for a winner
+        for (int i = 0; i < L; i++) {
+            if (board[i][0] != " " && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+                return (board[i][0] == "X") ? 1 : 2;
+            }
+            if (board[0][i] != " " && board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
+                return (board[0][i] == "X") ? 1 : 2;
+            }
         }
-        if (acc_uno == 3)
-            return 1;
-        
-        if (acc_due == 3)
-            return 2;
-        
-        return 0;
+        if (board[0][0] != " " && board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+            return (board[0][0] == "X") ? 1 : 2;
+        }
+        if (board[0][2] != " " && board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+            return (board[0][2] == "X") ? 1 : 2;
+        }
+        return 0;  // No winner yet
     }
-
-    int controlla_diagonali(){
-
-    }
-
 };
 
+int main() {
+    Tris game;
+    int x, y, currentPlayer = 1, movesLeft = 9;
+    bool validMove, gameOver = false;
 
-int main(int argc, char const *argv[])
-{
-    Tris myTris;
-    myTris.reset_tabella();
+    cout << "Welcome to the game of Tic-Tac-Toe (TRIS)!" << endl;
+    cout << "Player 1 (X) - Player 2 (O)" << endl;
 
-    cout << "Stato iniziale!" << endl;
-    myTris.stampa_griglia();
+    while (!gameOver && movesLeft > 0) {
+        game.printBoard();
+        cout << "Player " << currentPlayer << " - Enter your move (row column): ";
+        cin >> x >> y;
+        x--; y--;  // Adjust for 0-based indexing
 
-    int x, y;
-    bool mossa_valida;
-    int vincitore;
-    int mosse_totali = 0;
-
-    while (mosse_totali < 9)
-    {
-    do  {
-        cout << "Mossa del giocatore 1." << endl;
-        cout << "x: ";
-        cin >> x;
-
-        cout << "y: ";
-        cin >> y;
-
-        mossa_valida = myTris.giocatore_uno(x, y);
-
-    } while (!mossa_valida);
-
-    myTris.stampa_griglia();
-
-    vincitore = myTris.controlla_vincitore();
-    if(vincitore != 0)
-        break;
-
-    do  {
-        cout << "Mossa del giocatore 2." << endl;
-        cout << "x: ";
-        cin >> x;
-
-        cout << "y: ";
-        cin >> y;
-
-        mossa_valida = myTris.giocatore_due(x, y);
-
-    } while (!mossa_valida);
-    
-    myTris.stampa_griglia();
-    vincitore = myTris.controlla_vincitore();
-    if(vincitore != 0)
-        break;
-
-    mosse_totali = mosse_totali + 2;
+        validMove = game.placeSymbol(x, y, (currentPlayer == 1) ? "X" : "O");
+        if (validMove) {
+            movesLeft--;
+            int winner = game.checkWinner();
+            if (winner != 0) {
+                game.printBoard();
+                cout << "Congratulations! Player " << winner << " wins!" << endl;
+                gameOver = true;
+            } else if (movesLeft == 0) {
+                game.printBoard();
+                cout << "It's a draw! The game is over." << endl;
+                gameOver = true;
+            } else {
+                currentPlayer = (currentPlayer == 1) ? 2 : 1;
+            }
+        } else {
+            cout << "Invalid move! Please try again." << endl;
+        }
     }
-
-    if(vincitore == 1)
-        cout << "Vince giocatore 1!" << endl;
-    else if(vincitore == 2)
-        cout << "Vince giocatore 2!" << endl;
-    else
-        cout << " Pareggio!" << endl;
 
     return 0;
 }
+
