09a39b825d3464c8192adf1e22b12d90fe4594f9
JoshuaZirkzee <153986382+JoshuaZirkzee@users.noreply.github.com>
2024-03-02 11:39:03 +0000

    Gioco numeri sequenza

---
+++ X_MasterMind/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "mastermind.cpp"
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    mastermind prova = mastermind();
+
+    prova.nuova_giocata();
+
+    return 0;
+}
