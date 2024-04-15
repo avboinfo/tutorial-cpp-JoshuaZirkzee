249016a3b229354d7d67232752b292ed22b10feb
JoshuaZirkzee <153986382+JoshuaZirkzee@users.noreply.github.com>
2024-04-15 07:51:31 +0000

    15/04/2024

--- X_BattleShip/Matrice.cpp
+++ X_BattleShip/Matrice.cpp
@@ -1,35 +1,85 @@
 #include<iostream>
 using namespace std;
 
+const int DIM = 10;
+
 class Matrice {
 
     private:
-    int m[10][10];
+    char m[DIM][DIM];
 
     public:
+    Matrice(){
+        for(int i=0; i<DIM; i++){
+            for(int j=0; j<DIM; j++){
+                m[i][j] = 97 + rand()%26;
+        }
+     }
+}
     Matrice(int n) {
-        for(int i=0; i<10; i++){
-            for(int j=0; j<10; j++){
+        for(int i=0; i<DIM; i++){
+            for(int j=0; j<DIM; j++){
                 m[i][j] = n;
 
             }
         }
     }
 
+char get(int x, int y){
+    return m[x][y];
+}
+
+void get(int x, int y, char c){
+    return m[x][y] = c;
+}
     void stampa(){
         cout << endl << "--------------------------------" << endl;
-        for(int i=0; i<10; i++){
-            for(int j=0; j<10; j++){
-                cout << m[i][j] << "\t";
+        for(int i=0; i<DIM; i++){
+            for(int j=0; j<DIM; j++){
+                printf("%c ", m[i][j]);
             }
                 cout << endl;
         }
         cout << endl << "--------------------------------" << endl;
     }
+
+
+void bomb(){
+    int x = rand() % DIM;
+    int y = rand() % DIM;
+    m[x][y] = '*';
+}
+
+void placeHorizontalShip(int len){
+    if(len <= 0 || len >=DIM) return;
+    int x = rand() % DIM;
+    int y = rand() % (DIM - len);
+    for ( int i = 0; i < len; i++) m[x][y+i] = '0';
+}
+
+void placeVerticalShip(int len){
+    if(len <= 0 || len >=DIM) return;
+    int x = rand() % (DIM - len);
+    int y = rand() % DIM;
+    for ( int i = 0; i < len; i++) m[x + 1][y] = '0';
+}
+
 };
 
-
 int main(){
-    Matrice m1 = Matrice( 3 );
-    m1.stampa();
+    srand(time(NULL));
+    Matrice mappa = Matrice( '_' );
+    Matrice campo = Matrice('.');
+
+    campo.placeHorizontalShip(3);
+    campo.placeVerticalShip(4);
+    campo.placeVerticalShip(2);
+    campo.placeHorizontalShip(5);
+
+    for(int i = 0; i<20;i++){
+        int x = rand() % DIM;
+        int y = rand() % DIM;
+        if(campo.get(x,y)=='0') mappa.put(x,y,'*')
+    }
+    mappa.stampa();
 }
